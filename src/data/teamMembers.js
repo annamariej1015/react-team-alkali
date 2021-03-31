c7915cc1b9da43a5c8fa08b9ccf547f29d62f677
meru20 <merhawitgubsa@gmail.com>
2021-03-31 15:03:06 -0700

    added dataset to the web

---
+++ src/data/teamMembers.js
@@ -0,0 +1,96 @@
+export const membersData = [
+    {
+        firstName: 'Nakia',
+lastName: 'Callaway',
+email: 'nik.callaway@outlook.com',
+linkedIn: 'https://www.linkedin.com/in/nakia-callaway-31201b6/',
+github: 'https://github.com/nakiacallaway',
+bio: 'Coming to California is yet another stop on my long journey all over the country.  I recently moved for the 98th time.  My hobbies are woodworking, cooking, and gaming, both tabletop and computer.',
+profile_img: 'https://media-exp1.licdn.com/dms/image/C5603AQEJxnc9lSOCoQ/profile-displayphoto-shrink_[…]78400&v=beta&t=4rLw8CsI9swqad9snNQPeCPyXLpwBCyqGrTkGyysnFo',
+languages: ['html', 'css', 'javascript', 'react'],
+    },
+    {
+        firstName: 'Pardeep',
+        lastName: 'Sanghera',
+        email: 'pradeepsanghera@gmail.com',
+       linkedIn: 'linkedin.com/in/pardeep-sanghera-993b8a209,
+        github: 'https://github.com/Pardeepsanghera',
+        bio: 'Lorem ipsum',
+        profile_img: 'https://media-exp1.licdn.com/dms/image/C4E03AQHyFKY2L-h3Xw/profile-displayphoto-shrink_[…]78400&v=beta&t=q4gfJZVfvc9jqMy0gBYYZSxfXf1otB7hyQ3J-Pgb31I',
+        languages: ['html', 'css', 'javascript', 'react']
+    },
+    {
+        firstName: 'Alex',
+        lastName: 'Gutierrez',
+        email: 'agutierrez@bitwiseindustries.com',
+        linkedIn: 'https://www.linkedin.com/in/alex-gutierrez-mpa-6ba8bb34/',
+        github: 'https://github.com/alexg1021',
+        bio: 'lorem ipsum',
+        profile_img: 'https://media-exp1.licdn.com/dms/image/C5603AQH7DxGvg3R3XA/profile-displayphoto-shrink_200_200/0/1536703041727?e=1622678400&v=beta&t=eKhvifwFxw7KfJn2_a2OzUXVuixmsa8JlgPqo6YjuBY',
+        languages: ['html','css', 'javascript']
+    },
+    {
+        firstName: 'Merhawit',
+      lastName:'Gubsa',
+      email:'merhawitgubsa@gmail.com',
+      linkedIn:'https://www.linkedin.com/in/merhawit-gubsa/',
+      github:'https://github.com/meru20',
+      bio:'lorem ipsum',
+      profile_img:'https://media-exp1.licdn.com/dms/image/C5603AQG-X6R81-W7wA/profile-displayphoto-shrink_800_800/0/1617224479322?e=1622678400&v=beta&t=rEP8dIh2J41XRE3KZtUqb8d954YybPS3OCukwZEu1C4',
+      languages:['html','css','react','javascript','java']
+
+    },
+    {
+        firstName: 'Elizabeth',
+        lastName: 'Gaw',
+        email: 'hello.xyeras@gmail.com',
+        linkedIn: 'https://www.linkedin.com/in/elizabeth-agnes-gaw-6702a7139/',
+        github: 'https://github.com/xyeras',
+        bio: 'lorem ipsum',
+        profile_img: 'https://media-exp1.licdn.com/dms/image/C5603AQF0ROaUm_EXWw/profile-displayphoto-shrink_400_400/0/1611397157383?e=1622678400&v=beta&t=ajryz6YmZznohK-PD6Ltq63ExXHWUwm7wY54oqyO2BA',
+        languages: ['html','css', 'javascript', 'react', 'python', 'haskell', 'oz', 'c++']
+    },
+    {
+        firstName: 'Brittney',
+      lastName:'Cavazos',
+      email:'bcavazos@alphaworks.tech',
+      linkedIn:'https://www.linkedin.com/in/brittney-cavazos-83b056a9/',
+      github:'https://github.com/b-cavazos',
+      bio:'lorem ipsum',
+      profile_img:'https://media-exp1.licdn.com/dms/image/C5603AQHRFP4gIJp58g/profile-displayphoto-shrink_800_800/0/1578492310711?e=1622678400&v=beta&t=0HRaphHlpZn0giNFr799ZJyqimnglvrWnmKaKM-E9aM',
+      languag
+    },
+    {
+        firstName: 'Jeffrey',
+        lastName: 'Rios',
+        email: 'jeffriosjr@gmail.com',
+        linkedIn: 'https://www.linkedin.com/in/jeffrey-rios-a113a949/',
+        github: 'https://github.com/jeffreyriosjr',
+        bio: 'lorem ipsum',
+        profile_img: 'https://media-exp1.licdn.com/dms/image/C5603AQE-EURX3_t5DA/profile-displayphoto-shrink_200_200/0/1616453773735?e=1622678400&v=beta&t=BA_m1wLet2-8HhMFEkl9v5I5I1rCpMAP1G2_H3CpMgY',
+        languages: ['html','css', 'javascript', 'react']
+  
+    },
+    {
+        firstName: 'Ludwin',
+        lastName: 'Granados',
+        email: 'ludwingranados@gmail.com',
+        linkedIn: 'linkedin.com/in/ludwin-granados-358535195',
+        github: 'https://github.com/ludgranados',
+        bio: 'Love fishing, working on a aquaterrarium tank, building things, and reading. "walks on the beach jk.."',
+        profile_img: 'https://i.ibb.co/vQM2k7C/Hero-image.jpg',
+        languages: ['html', 'css', 'javascript', 'react']
+
+    },
+    {
+        firstName: 'Anna',
+        lastName:'Jimenez',
+         email: 'annamariej1015@gmail.com',
+   linkedIn: 'https://www.linkedin.com/in/annamarie-jimenez-231b79182/',
+   github: 'https://github.com/annamariej1015',
+   bio: 'lorem ipsum',
+   profile_img: 'https://media-exp1.licdn.com/dms/image/C5603AQEH3UH2jdXzdQ/profile-displayphoto-shrink_400_400/0/1611110839376?e=1622678400&v=beta&t=zOKhMYmgPrDbPa4bZOIrHyp2S4Y-1uRTIgFwNmA1tgs',
+   languages: ['html', 'css', 'javascript', 'react', 'react native ', 'nodeJS', 'mongoDB', 'express']
+
+    }
+]
