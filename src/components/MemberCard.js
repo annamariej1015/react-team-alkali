afc19506d2f8ceee71ef7645078cce9f6c9a6366
Anna <annamariej1015@gmail.com>
2021-04-05 18:36:45 -0500

    search bar to render members

--- src/components/MemberCard.js
+++ src/components/MemberCard.js
@@ -4,24 +4,23 @@ import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const MemberCard =({member}) =>{
- 
+
     return(
-       
+    
                 
                     <div className='card mb-3'>
                         <div className='card-header text-center'>
                             {member.role}
                             </div>
                         
-                       
                             <div className='th-card-bg-img' style={{backgroundImage:`url(${member.profile_img})`}}>
 
                             </div>
-                           
+                        
                             <div className='card-body'>
                                 <h4 className='card-title'>{member.firstName} {member.lastName}</h4>
                                 {/* <p className='card-text'>{member.bio}</p> */}
-                              
+                            
                                     <div className='my-2'>
                                         <strong>Languages:</strong>
                                         {member.languages.map((language,i) => {
