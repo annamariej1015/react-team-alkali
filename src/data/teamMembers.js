f793e63c9a05fa961a4031b9b0cc68a843c32b1a
meru20 <merhawitgubsa@gmail.com>
2021-04-05 13:24:03 -0700

    feat(members,teamMembers): added role to the dataset and added member card feature to the members page

--- src/data/teamMembers.js
+++ src/data/teamMembers.js
@@ -7,7 +7,8 @@ export const membersData = [
         linkedIn: 'https://www.linkedin.com/in/nakia-callaway-31201b6/',
         github: 'https://github.com/nakiacallaway',
         bio: 'Coming to California is yet another stop on my long journey all over the country.  I recently moved for the 98th time.  My hobbies are woodworking, cooking, and gaming, both tabletop and computer.',
-        profile_img: 'https://media-exp1.licdn.com/dms/image/C5603AQEJxnc9lSOCoQ/profile-displayphoto-shrink_[…]78400&v=beta&t=4rLw8CsI9swqad9snNQPeCPyXLpwBCyqGrTkGyysnFo',
+        profile_img: 'https://media-exp1.licdn.com/dms/image/C5603AQEJxnc9lSOCoQ/profile-displayphoto-shrink_800_800/0/1611900669466?e=1622678400&v=beta&t=4rLw8CsI9swqad9snNQPeCPyXLpwBCyqGrTkGyysnFo',
+        role: 'React Apprentice',
         languages: ['html', 'css', 'javascript', 'react'],
     },
     {
@@ -17,8 +18,9 @@ export const membersData = [
         email: 'pradeepsanghera@gmail.com',
         linkedIn: 'linkedin.com/in/pardeep-sanghera-993b8a209',
         github: 'https://github.com/Pardeepsanghera',
-        bio: 'Lorem ipsum',
-        profile_img: 'https://media-exp1.licdn.com/dms/image/C4E03AQHyFKY2L-h3Xw/profile-displayphoto-shrink_[…]78400&v=beta&t=q4gfJZVfvc9jqMy0gBYYZSxfXf1otB7hyQ3J-Pgb31I',
+        bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        profile_img: 'https://media-exp1.licdn.com/dms/image/C4E03AQHyFKY2L-h3Xw/profile-displayphoto-shrink_200_200/0/1616519627952?e=1622678400&v=beta&t=q4gfJZVfvc9jqMy0gBYYZSxfXf1otB7hyQ3J-Pgb31I',
+        role: 'React Apprentice',
         languages: ['html', 'css', 'javascript', 'react']
     },
     {
@@ -28,8 +30,9 @@ export const membersData = [
         email: 'agutierrez@bitwiseindustries.com',
         linkedIn: 'https://www.linkedin.com/in/alex-gutierrez-mpa-6ba8bb34/',
         github: 'https://github.com/alexg1021',
-        bio: 'lorem ipsum',
+        bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         profile_img: 'https://media-exp1.licdn.com/dms/image/C5603AQH7DxGvg3R3XA/profile-displayphoto-shrink_200_200/0/1536703041727?e=1622678400&v=beta&t=eKhvifwFxw7KfJn2_a2OzUXVuixmsa8JlgPqo6YjuBY',
+        role: 'React Apprenticeship leader',
         languages: ['html','css', 'javascript']
     },
     {
@@ -39,8 +42,9 @@ export const membersData = [
         email:'merhawitgubsa@gmail.com',
         linkedIn:'https://www.linkedin.com/in/merhawit-gubsa/',
         github:'https://github.com/meru20',
-        bio:'lorem ipsum',
+        bio:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         profile_img:'https://media-exp1.licdn.com/dms/image/C5603AQG-X6R81-W7wA/profile-displayphoto-shrink_800_800/0/1617224479322?e=1622678400&v=beta&t=rEP8dIh2J41XRE3KZtUqb8d954YybPS3OCukwZEu1C4',
+        role: 'React Apprentice',
         languages:['html','css','react','javascript','java']
 
     },
@@ -51,8 +55,9 @@ export const membersData = [
         email: 'hello.xyeras@gmail.com',
         linkedIn: 'https://www.linkedin.com/in/elizabeth-agnes-gaw-6702a7139/',
         github: 'https://github.com/xyeras',
-        bio: 'lorem ipsum',
+        bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         profile_img: 'https://media-exp1.licdn.com/dms/image/C5603AQF0ROaUm_EXWw/profile-displayphoto-shrink_400_400/0/1611397157383?e=1622678400&v=beta&t=ajryz6YmZznohK-PD6Ltq63ExXHWUwm7wY54oqyO2BA',
+        role: 'React Apprentice',
         languages: ['html','css', 'javascript', 'react', 'python', 'haskell', 'oz', 'c++']
     },
     {
@@ -62,8 +67,9 @@ export const membersData = [
         email:'bcavazos@alphaworks.tech',
         linkedIn:'https://www.linkedin.com/in/brittney-cavazos-83b056a9/',
         github:'https://github.com/b-cavazos',
-        bio:'lorem ipsum',
+        bio:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         profile_img:'https://media-exp1.licdn.com/dms/image/C5603AQHRFP4gIJp58g/profile-displayphoto-shrink_800_800/0/1578492310711?e=1622678400&v=beta&t=0HRaphHlpZn0giNFr799ZJyqimnglvrWnmKaKM-E9aM',
+        role: 'React Apprentice',
         languages:['html','css','javascript', 'react']
     },
     {
@@ -73,8 +79,9 @@ export const membersData = [
         email: 'jeffriosjr@gmail.com',
         linkedIn: 'https://www.linkedin.com/in/jeffrey-rios-a113a949/',
         github: 'https://github.com/jeffreyriosjr',
-        bio: 'lorem ipsum',
+        bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         profile_img: 'https://media-exp1.licdn.com/dms/image/C5603AQE-EURX3_t5DA/profile-displayphoto-shrink_200_200/0/1616453773735?e=1622678400&v=beta&t=BA_m1wLet2-8HhMFEkl9v5I5I1rCpMAP1G2_H3CpMgY',
+        role: 'React Apprentice',
         languages: ['html','css', 'javascript', 'react']
   
     },
@@ -87,6 +94,7 @@ export const membersData = [
         github: 'https://github.com/ludgranados',
         bio: 'Love fishing, working on a aquaterrarium tank, building things, and reading. "walks on the beach jk.."',
         profile_img: 'https://i.ibb.co/vQM2k7C/Hero-image.jpg',
+        role: 'React Apprentice',
         languages: ['html', 'css', 'javascript', 'react']
 
     },
@@ -97,8 +105,9 @@ export const membersData = [
         email: 'annamariej1015@gmail.com',
         linkedIn: 'https://www.linkedin.com/in/annamarie-jimenez-231b79182/',
         github: 'https://github.com/annamariej1015',
-        bio: 'lorem ipsum',
+        bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         profile_img: 'https://media-exp1.licdn.com/dms/image/C5603AQEH3UH2jdXzdQ/profile-displayphoto-shrink_400_400/0/1611110839376?e=1622678400&v=beta&t=zOKhMYmgPrDbPa4bZOIrHyp2S4Y-1uRTIgFwNmA1tgs',
+        role: 'React Apprentice',
         languages: ['html', 'css', 'javascript', 'react', 'react native ', 'nodeJS', 'mongoDB', 'express']
 
     },
@@ -110,8 +119,9 @@ export const membersData = [
         email: 'hstamps4@gmail.com',
         linkedIn: 'https://www.linkedin.com/in/harrison-stamps-201184142/',
         github: 'https://github.com/hstamps-stack',
-        bio: 'lorem ipsum',
-        profile_img: 'https://ibb.co/cwz8zv9',
+        bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        profile_img: 'https://media-exp1.licdn.com/dms/image/C5603AQGXMfWZzwMopA/profile-displayphoto-shrink_200_200/0/1616791326115?e=1622678400&v=beta&t=NfnQ4TAUTfkEIsA2gt_HBsv_wOLJ22NAfYDW4zDhsUc',
+        role: 'React Apprentice',
         languages: ['html','css', 'javascript', 'react','react native ', 'nodeJS', 'mongoDB', 'express']
         
     },
@@ -122,8 +132,9 @@ export const membersData = [
         email: 'pravallikaammineni@gmail.com',
         linkedIn: 'https://www.linkedin.com/in/pravalika-a-12aa9b18a/',
         github: 'https://github.com/pravallika89',
-        bio: 'Lorem ipsum',
+        bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         profile_img: 'https://ca.slack-edge.com/T03A6AQRX-U01MRFW8PGA-e931aafaaa4d-512',
+        role: 'React Apprentice',
         languages: ['html', 'css', 'javascript', 'react']
         
     }
